19aba53f4f3b35d2b553ba5e4f0e0a280ebdc28c
RoganInglis <rogan27@live.co.uk>
2017-09-22 22:28:19 +0100

    Setup - to complete

---
+++ agents/__init__.py
@@ -0,0 +1,19 @@
+from agents.base_agent import BaseAgent
+
+__all__ = [
+    "BaseAgent",
+]
+
+
+def make_agent(config):
+    if config['agent_name'] in __all__:
+        return globals()[config['agent_name']](config)
+    else:
+        raise Exception('The agent name %s does not exist' % config['agent_name'])
+
+
+def get_agent_class(config):
+    if config['model_name'] in __all__:
+        return globals()[config['model_name']]
+    else:
+        raise Exception('The agent name %s does not exist' % config['agent_name'])
